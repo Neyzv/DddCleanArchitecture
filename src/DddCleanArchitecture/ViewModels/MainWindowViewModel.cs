d66418c40547ab49a5d838d882403985b9e5033d
Neyzu <furyax13@gmail.com>
2025-11-11 14:28:46 +0100

    feat(resources): adding resources and base text sizes

--- src/DddCleanArchitecture/ViewModels/MainWindowViewModel.cs
+++ src/DddCleanArchitecture/ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,10 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using DddCleanArchitecture.Internationalisation.Resources;
 
 namespace DddCleanArchitecture.ViewModels;
 
 public sealed partial class MainWindowViewModel
     : ObservableObject
 {
-    [ObservableProperty] private string _greetings = "Hello from DDD architecture !";
+    [ObservableProperty] private string _homeLabel = Labels.Home;
 }
