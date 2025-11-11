cb5b980ae6d3a6b34cae856afd363535d09a8a9d
Neyzu <furyax13@gmail.com>
2025-11-11 15:20:59 +0100

    feat(navigation): main view navigation

--- src/DddCleanArchitecture/ViewModels/MainWindowViewModel.cs
+++ src/DddCleanArchitecture/ViewModels/MainWindowViewModel.cs
@@ -1,10 +1,34 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System.Windows.Controls;
+using CommunityToolkit.Mvvm.ComponentModel;
 using DddCleanArchitecture.Internationalisation.Resources;
+using DddCleanArchitecture.Services.Navigation;
+using DddCleanArchitecture.Views;
 
 namespace DddCleanArchitecture.ViewModels;
 
 public sealed partial class MainWindowViewModel
     : ObservableObject
 {
+    private readonly INavigationService _navigationService;
+
     [ObservableProperty] private string _homeLabel = Labels.Home;
+    [ObservableProperty] private Control? _current;
+
+    public MainWindowViewModel()
+    {
+        _navigationService = null!;
+    }
+
+    public MainWindowViewModel(INavigationService navigationService)
+    {
+        _navigationService = navigationService;
+
+        _navigationService.ViewChanged += OnCurrentViewChanged;
+        _navigationService.NavigateTo<HomeView>();
+    }
+
+    private void OnCurrentViewChanged(Control newView)
+    {
+        Current = newView;
+    }
 }
