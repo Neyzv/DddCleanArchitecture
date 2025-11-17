8b9f3dfbf25fd521a0f5f8c4ad772b2d75092670
Neyzu <furyax13@gmail.com>
2025-11-17 10:24:36 +0100

    feat(i18n): internationalisation

---
+++ src/DddCleanArchitecture/Converters/MultiplyConverter.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace DddCleanArchitecture.Converters;
+
+public sealed class MultiplyConverter : IValueConverter
+{
+    public double Factor { get; set; } = 1.0;
+
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture) =>
+        value is double d ? d * Factor : value;
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) =>
+        value is double d ? d / Factor : value;
+}
--- src/DddCleanArchitecture/ViewModels/MainWindowViewModel.cs
+++ src/DddCleanArchitecture/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,8 @@
 ﻿using System.Windows.Controls;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using DddCleanArchitecture.Domain.Enums;
+using DddCleanArchitecture.Domain.Services.Internationalisation;
 using DddCleanArchitecture.Internationalisation.Resources;
 using DddCleanArchitecture.Services.Navigation;
 using DddCleanArchitecture.Views;
@@ -10,25 +13,47 @@ public sealed partial class MainWindowViewModel
     : ObservableObject
 {
     private readonly INavigationService _navigationService;
+    private readonly IInternationalisationService _internationalisationService;
 
-    [ObservableProperty] private string _homeLabel = Labels.Home;
     [ObservableProperty] private Control? _current;
+    
+#pragma warning disable CA1822
+    public string HomeLabel =>
+#pragma warning restore CA1822
+        Labels.Home;
 
     public MainWindowViewModel()
     {
         _navigationService = null!;
+        _internationalisationService = null!;
     }
 
-    public MainWindowViewModel(INavigationService navigationService)
+    public MainWindowViewModel(INavigationService navigationService, IInternationalisationService internationalisationService)
     {
         _navigationService = navigationService;
+        _internationalisationService = internationalisationService;
 
         _navigationService.ViewChanged += OnCurrentViewChanged;
         _ = _navigationService.NavigateToAsync<HomeView>();
+
+        _internationalisationService.LanguageChanged += OnLanguageChanged;
+    }
+
+    private void OnLanguageChanged()
+    {
+        OnPropertyChanged(nameof(HomeLabel));
     }
 
     private void OnCurrentViewChanged(Control newView)
     {
         Current = newView;
     }
+
+    [RelayCommand]
+    private void ChangeLanguageToFrench() =>
+        _internationalisationService.TryChangeLanguage(Language.French);
+
+    [RelayCommand]
+    private void ChangeLanguageToEnglish() =>
+        _internationalisationService.TryChangeLanguage(Language.English);
 }
