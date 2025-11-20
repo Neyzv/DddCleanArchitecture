c456f8e7bb442ee15a4dcb11004b5c9e5f3b52bb
Neyzu <furyax13@gmail.com>
2025-11-20 09:47:14 +0100

    doc(navigable vm): providing some doc to the navigable view model

--- src/DddCleanArchitecture/ViewModels/Navigation/NavigableViewModel.cs
+++ src/DddCleanArchitecture/ViewModels/Navigation/NavigableViewModel.cs
@@ -1,7 +1,13 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using DddCleanArchitecture.Domain.Attributes;
 
 namespace DddCleanArchitecture.ViewModels.Navigation;
 
+/// <summary>
+/// Navigable view model which needs to be used with the <see cref="NavigableForAttribute"/> to be registered in the navigation repository.<br/>
+/// <br/>
+/// <b>NB:</b> You don't need to register or inject the view model because it will be created as a singleton instance by the repository.
+/// </summary>
 public abstract class NavigableViewModel
     : ObservableObject
 {
