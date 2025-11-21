3e85931313d886eec86b118834a02ade99d2c84b
Neyzu <furyax13@gmail.com>
2025-11-21 14:29:40 +0100

    feat(article/comments): comments rendering

---
+++ src/DddCleanArchitecture/Models/Articles/CommentPresenter.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Media;
+
+namespace DddCleanArchitecture.Models.Articles;
+
+public sealed record CommentPresenter(string Content, DateTime CreatedOn)
+{
+    public SolidColorBrush IllusColor { get; } =
+        new(Color.FromRgb((byte)Random.Shared.Next(0, 255), (byte)Random.Shared.Next(0, 255), (byte)Random.Shared.Next(0, 255)));
+}
--- src/DddCleanArchitecture/ViewModels/ArticleViewModel.cs
+++ src/DddCleanArchitecture/ViewModels/ArticleViewModel.cs
@@ -1,6 +1,8 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
 using DddCleanArchitecture.Domain.Attributes;
 using DddCleanArchitecture.Domain.Models.Articles;
+using DddCleanArchitecture.Models.Articles;
 using DddCleanArchitecture.ViewModels.Navigation;
 using DddCleanArchitecture.Views;
 
@@ -11,4 +13,17 @@ public sealed partial class ArticleViewModel
     : NavigableViewModel
 {
     [ObservableProperty] private Article? _article;
+
+    public ObservableCollection<CommentPresenter> Comments { get; } = [];
+
+    partial void OnArticleChanged(Article? value)
+    {
+        Comments.Clear();
+
+        if (value is null)
+            return;
+
+        foreach (var comment in value.Comments)
+            Comments.Add(new CommentPresenter(comment.Content, comment.CreatedOn));
+    }
 }
